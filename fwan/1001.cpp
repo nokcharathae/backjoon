33dddc45c0983a3276599b81c03e8c9a77714544
chltnqhd1 <chltnqhd1234@naver.com>
2023-11-03 02:23:56 +0900

    개짜증

--- fwan/1001.cpp
+++ fwan/1001.cpp
@@ -3,12 +3,17 @@
 using namespace std;
 
 int main() {
-    string s;
-    cin >> s;
-    int result,n = s.length()/2;
-        for(int i=0;i<n;i++){
-            if(s[i]==s[s.length()-1-i]) result = 1;
-            else {result=0; break;}
-        }
-    cout << result << endl;
+    int A[9][9];
+    for(int i=0;i<9;i++)
+        for(int j=0;j<9;j++)
+            cin >> A[i][j];
+    int a = A[0][0], x=0,y=0;
+    for(int i=0;i<9;i++)
+            for(int j=0;j<9;j++)
+            if(a<A[i][j]){
+                a = A[i][j];
+                x = i;
+                y = j;
+            }
+    cout << a << endl << x+1 << " " << y+1;
 }
