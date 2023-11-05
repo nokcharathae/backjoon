89ca82ed9050201918139d07f323919de6b98a1f
chltnqhd1 <chltnqhd1234@naver.com>
2023-11-06 02:11:56 +0900

    1106

--- fwan/1001.cpp
+++ fwan/1001.cpp
@@ -1,19 +1,7 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include<cmath>
 using namespace std;
 
-int main() {
-    int A[9][9];
-    for(int i=0;i<9;i++)
-        for(int j=0;j<9;j++)
-            cin >> A[i][j];
-    int a = A[0][0], x=0,y=0;
-    for(int i=0;i<9;i++)
-            for(int j=0;j<9;j++)
-            if(a<A[i][j]){
-                a = A[i][j];
-                x = i;
-                y = j;
-            }
-    cout << a << endl << x+1 << " " << y+1;
+int main(){
+    cout << pow(36,5);
 }
