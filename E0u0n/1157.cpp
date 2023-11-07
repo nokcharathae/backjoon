9d511e45460a366763dcbc2152edffb78d6e0c46
nokcharathae <rkwmal0000@naver.com>
2023-11-07 23:21:26 +0900

    20231106

--- E0u0n/1157.cpp
+++ E0u0n/1157.cpp
@@ -6,12 +6,32 @@ int main(){
     ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
     
     string s;
+    int c,num=0;
     cin>>s;
+    int a[s.length()]={0,};
 
-    for(int i=0; i<s.length(); i++)
-    {
-        
+    for(int i=1; i<s.length(); i++)
+    {        
+        for(int j=0; j<i; j++)
+        {
+            
+            if(s[j]==s[i] or s[j]==s[i]-32 or s[j]==s[i]+32)
+            {
+                a[j]++;
+                a[i]++;
+                c=s[j];
+                cout<<s[j]<<'\n';
+                cout<<i<<", "<<j<<", "<<a[j]<<'\n';
+            }
+            if(num<a[j])
+                num=a[j];
+        }
     }
+
+    if(c>=97)
+        cout<<char(c-32);
+    else
+        cout<<char(c);
 
     return 0;
 }
